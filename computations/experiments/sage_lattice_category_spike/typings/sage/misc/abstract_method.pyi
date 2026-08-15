9a839072133135dadddd6b3ea97a6366be43d83b
Zack Garza <dzackgarza@gmail.com>
2026-08-15 20:48:15 +0800

    Use shared Sage stubs

--- computations/experiments/sage_lattice_category_spike/typings/sage/misc/abstract_method.pyi
+++
@@ -1,7 +0,0 @@
-# Repo-scoped stubs; see lexicon/README.md.
-from collections.abc import Callable
-from typing import Any, TypeVar
-
-_F = TypeVar("_F", bound=Callable[..., Any])
-
-def abstract_method(f: _F) -> _F: ...
