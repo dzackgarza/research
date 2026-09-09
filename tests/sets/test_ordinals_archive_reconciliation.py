28054cdedcf9cced0fcddd10d6ed9d0f3051a409
D. Zack Garza <dzackgarza@gmail.com>
2026-09-09 15:20:09 +0000

    test(sets): reconcile archived ordinal semantics [unverified]

---
+++ tests/sets/test_ordinals_archive_reconciliation.py
@@ -0,0 +1,33 @@
+from dzack_research.preamble.all import Ordinals, omega
+
+
+def test_archive_initial_ordinals_retain_their_index() -> None:
+    ordinals = Ordinals()
+    index = omega(1).ordinal_sum(3)
+    initial = ordinals.initial(index)
+
+    assert initial.is_initial()
+    assert initial.initial_index() == index
+    assert initial.cardinality().initial_ordinal() == initial
+
+
+def test_archive_natural_and_ordinary_ordinal_products_are_distinct() -> None:
+    one = Ordinals().one()
+    omega0 = omega(0)
+
+    assert one * omega0 == omega0 * one
+    assert one.ordinal_product(omega0) == omega0
+    assert omega0.ordinal_product(one) == omega0
+
+    two = Ordinals()(2)
+    assert two * omega0 == omega0 * two
+    assert two.ordinal_product(omega0) != omega0.ordinal_product(two)
+
+
+def test_archive_ordinary_ordinal_power_retains_nonfinite_structure() -> None:
+    omega0 = omega(0)
+    exponent = Ordinals()(2)
+    power = omega0.ordinal_power(exponent)
+
+    assert power != omega0
+    assert power.cardinality() == omega0.cardinality() ** exponent.cardinality()
