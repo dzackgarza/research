bfb7d673a607ddd444f5f1075fc23cd7b7e878b8
D. Zack Garza <dzackgarza@gmail.com>
2026-09-09 14:57:41 +0000

    test(schemes): reconcile ringed-space archive [unverified]

---
+++ tests/schemes/test_ringed_spaces_archive.py
@@ -0,0 +1,32 @@
+"""Archive reconciliation for ringed and locally ringed spaces."""
+
+from dzack_research.preamble.all import (
+    AffineSpace,
+    LocalRings,
+    LocallyRingedSpaces,
+    QQ,
+    RingedSpaces,
+)
+
+
+def test_archived_ringed_space_structure_is_live_on_affine_schemes() -> None:
+    line = AffineSpace(1, QQ, names=("x",))
+    sheaf = line.structure_sheaf()
+
+    assert line in RingedSpaces()
+    assert line in LocallyRingedSpaces()
+    assert line.underlying_space() is line.underlying_space()
+    assert sheaf.ringed_space() is line
+    assert sheaf.global_sections() is line.coordinate_ring()
+
+
+def test_archived_locally_ringed_stalk_is_the_actual_point_local_ring() -> None:
+    line = AffineSpace(1, QQ, names=("x",))
+    algebra = line.coordinate_ring()
+    x = algebra.algebra_generator("x")
+    point = line.underlying_space()(algebra.ideal(x))
+
+    stalk = line.stalk(point)
+    assert stalk is line.structure_sheaf().stalk(point)
+    assert stalk is point.local_ring()
+    assert stalk in LocalRings()
