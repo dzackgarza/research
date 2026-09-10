856a04d7297dcf38b66244672b6bc5ca01e2c9ce
D. Zack Garza <dzackgarza@gmail.com>
2026-09-10 13:27:31 +0000

    test(archive): reconcile D4 triality subgroup orbits [unverified]

--- tests/lattices/test_discriminant_actions_archive.py
+++ tests/lattices/test_discriminant_actions_archive.py
@@ -39,3 +39,15 @@ def test_d4_weyl_group_is_trivial_on_glue_but_triality_is_not() -> None:
     assert full_image.cardinality() == 6
     assert form.orthogonal_group().cardinality() == 6
     assert set(full_image.orbit(generator)) == nonzero
+
+
+def test_d4_triality_fuses_the_three_order_two_glue_subgroups() -> None:
+    lattice = Lattices.D4
+    form = lattice.discriminant_group()
+    triality = lattice.Aut().discriminant_image()
+    subgroup_orbits = tuple(form.orbits_on_subobjects(group=triality))
+    isotropic_orbits = tuple(form.orbits_on_isotropic_subobjects(group=triality))
+
+    assert sorted(len(orbit) for orbit in subgroup_orbits) == [1, 1, 3]
+    assert len(isotropic_orbits) == 1
+    assert len(isotropic_orbits[0]) == 1
